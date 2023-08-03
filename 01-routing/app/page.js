891dae21ee37b75271a3f6021bdcd74c8aa0534d
SelimGüney <slmgny333@gmail.com>
2023-08-03 19:50:56 +0300

    Next Link

--- 01-routing/app/page.js
+++ 01-routing/app/page.js
@@ -1,3 +1,22 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <div>Hello Next</div>
+  return (
+    <div>
+      <Link href="/about">About</Link>
+      <br />
+      <Link href="/docs/post-1">Post 1</Link>
+      <br />
+      <Link href="/docs/post-1/post-2">Post 2</Link>
+      <br />
+      <Link
+        className="test"
+        href={{ pathname: "/about", query: { name: "Next.js", surname: "ff" } }}
+        replace={false} //default is false, if true it doesnt send the page before to the history.
+        prefetch={true} //default is true, it provides fetching the page before clicking
+      >
+        About with object
+      </Link>
+    </div>
+  )
 }
