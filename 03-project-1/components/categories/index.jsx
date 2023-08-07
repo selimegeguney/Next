a641fb9633dd812871dbf17e68aaf1fa4e844af0
SelimGüney <slmgny333@gmail.com>
2023-08-07 19:03:34 +0300

    featured-movie & categories

---
+++ 03-project-1/components/categories/index.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import React from "react"
+import styles from "./styles.module.css"
+const Categories = ({ categories }) => {
+  return (
+    <div className={styles.categories}>
+      {categories.map((category) => (
+        <Link
+          key={category.id}
+          className={styles.category}
+          href={`/${category.id}`}
+        >
+          <div className={styles.name}>{category.name}</div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Categories
--- 03-project-1/containers/home/index.jsx
+++ 03-project-1/containers/home/index.jsx
@@ -1,7 +1,14 @@
-import React from "react"
-
+import FeaturedMovie from "@/components/featured-movie"
+import Movies from "@/mocks/movies.json"
+import Categories from "@/components/categories"
+import Genres from "@/mocks/genres.json"
 const HomeContainer = () => {
-  return <div>HomeContainer</div>
+  return (
+    <div>
+      <FeaturedMovie movie={Movies.results[9]} />
+      <Categories categories={Genres.genres.slice(0, 5)} />
+    </div>
+  )
 }
 
 export default HomeContainer
