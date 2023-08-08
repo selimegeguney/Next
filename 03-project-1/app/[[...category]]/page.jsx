119178bd55655affb4b2f7b80d1feee4b44412a6
SelimGüney <slmgny333@gmail.com>
2023-08-08 17:33:53 +0300

    category section

---
+++ 03-project-1/app/[[...category]]/page.jsx
@@ -0,0 +1,18 @@
+import HomeContainer from "@/containers/home"
+
+import Movies from "@/mocks/movies.json"
+
+export default function Home({ params }) {
+  let selectedCategory
+  if (params.category?.length > 0) {
+    selectedCategory = true
+  }
+  return (
+    <HomeContainer
+      selectedCategory={{
+        id: params.category?.[0] ?? "",
+        movies: selectedCategory ? Movies.results.slice(13, 19) : [],
+      }}
+    />
+  )
+}
--- 03-project-1/containers/home/index.jsx
+++ 03-project-1/containers/home/index.jsx
@@ -4,11 +4,21 @@ import MoviesSection from "@/components/movies-section"
 
 import Movies from "@/mocks/movies.json"
 import Genres from "@/mocks/genres.json"
-const HomeContainer = () => {
+const HomeContainer = ({ selectedCategory }) => {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[9]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      {selectedCategory.id && (
+        <MoviesSection
+          title={
+            Genres.genres.find(
+              (category) => `${category.id}` === selectedCategory.id
+            ).name
+          }
+          movies={selectedCategory.movies}
+        />
+      )}
       <MoviesSection
         title="POPULAR  FILMS"
         movies={Movies.results.slice(1, 7)}
